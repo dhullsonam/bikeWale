c2c7a67df1fad58ebbf1e4c309aede42851bf84f
sonam <sonamdhull001@outlook.com>
2021-12-14 23:13:51 +0530

    tried addition and deletion of Repo

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,11 +1,10 @@
 
-
 import React from 'react';
 
 function HomePage() {
  return(
      <div>
-         <img src="https://wallpapercave.com/wp/TFKcm5v.jpg" style={{width :"100%" , height:"70vh" }}></img>
+         <h1>home page</h1>
      </div>
  )
 }
--- src/components/Logo/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-function Logo() {
-    return(
-        <h1>BikeWale</h1>
-    )
-}
-export default Logo;
