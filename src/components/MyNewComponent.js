5ea39928d8d28736707ba5bf22f6c56e168f0426
sonam <sonamdhull001@outlook.com>
2021-12-18 22:15:22 +0530

    show data in list

--- src/components/MyNewComponent.js
+++ src/components/MyNewComponent.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import ChlidComponent from "./ChlidComponent";
-import {logDOM} from "@testing-library/react";
+
 
 function MyNewComponent() {
+    const [item ,setItem] =useState([])
     const handleMyEvent = ()=>{
         console.log("parent")
         fetch("https://api.github.com/users/dhullsonam/repos")
             .then(response=>response.json())
-            .then(data=>{console.log(data[0].clone_url)
-                console.log(data[0].full_name)})
+            .then(data =>setItem(data))
     }
 
     return (
         <div>
             <ChlidComponent myInfo={handleMyEvent}/>
+            <ol>
+                {item.map(i=>{
+                    return <li><h4>{i.full_name}</h4></li>
+                })}
+            </ol>
         </div>
     )
 }
--- src/components/test.js
+++ src/components/test.js
@@ -1,1 +1,7 @@
- //e
+  const items = [2,4,5,6,7,8,9,10,11]
+
+   const result = items.reduce( (a,b) => a+b)
+  console.log(result)
+
+  const re = items.filter(i => i%2 ===0)
+  console.log(re)
